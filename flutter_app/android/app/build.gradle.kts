2d625a2c23b38d134f2d670e81b97e6f61193401
Manas <manasdey.iron006@gmail.com>
2026-09-04 23:57:07 +0530

    build: upgrade targetSdk to 36 and increment build version to 1.0.0+2

--- flutter_app/android/app/build.gradle.kts
+++ flutter_app/android/app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
     defaultConfig {
         applicationId = "com.kaundega.kaun_dega"
         minSdk = flutter.minSdkVersion
-        targetSdk = 35
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
